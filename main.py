dfbe15b3c9a88b24e7c8d8be503b67122acce6cd
ungcas000 <cassandraung@wireless-10-148-137-110.public.utexas.edu>
2015-09-26 23:30:48 -0500

    connected home and result page

--- main.py
+++ main.py
@@ -15,11 +15,23 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('templates/home.html')
+        self.response.write(template.render())
+
+class ResultHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/result.html')
+        self.response.write(template.render())
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/result') ResultHandler
 ], debug=True)
