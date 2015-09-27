ce298618d85affd523ec0a0bf2328c51a4ce9f08
Geronimo Lopez <geronimo@wireless-10-148-115-70.public.utexas.edu>
2015-09-27 00:52:53 -0500

    about to merge

--- main.py
+++ main.py
@@ -15,10 +15,36 @@
 # limitations under the License.
 #
 import webapp2
+import json
+from google.appengine.api import urlfetch
+import jinja2
+import os
+import logging
+import random
+
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.out.write(template.render())
+
+    def post(self):
+        raw_term = self.request.get('term', default_value="hunger games")
+        term = raw_term.replace(" ", "+")
+        if term == "":
+            term = "can+you+not"
+            pass
+        food_source = urlfetch.fetch(
+            'https://service.livestrong.com/service/food/foods/?query=' + term + '&limit=1&fill=cals')
+        food_JSON = food_source.content
+        calorie_dict = json.loads(food_JSON)
+        self.response.write(int(calorie_dict['foods'][0]['cals']))
+
+
+        #self.response.write("<html>" + calories + "</html>")
+       
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
